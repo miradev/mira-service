ca8c2f2071367f51d6b89b4563351ae0e7995fc6
Harry Huang <sam.harry.huang@gmail.com>
2019-12-31 01:34:34 -0500

    IT WORKS!!! Remove debug

--- src/clients/mongodb.ts
+++ src/clients/mongodb.ts
@@ -3,7 +3,6 @@ import { Db, MongoClient } from "mongodb"
 
 const uri = process.env.MONGODB_URI || config.get<string>("database.uri")
 const dbName = config.get<string>("database.name")
-console.log("URI", uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 let connection: MongoClient
 
