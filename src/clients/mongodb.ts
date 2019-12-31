d459e1087b8e594f78678a81a193b9710cacbf84
Harry Huang <sam.harry.huang@gmail.com>
2019-12-31 00:52:33 -0500

    For debugging on CloudWatch

--- src/clients/mongodb.ts
+++ src/clients/mongodb.ts
@@ -3,6 +3,7 @@ import { Db, MongoClient } from "mongodb"
 
 const uri = process.env.MONGODB_URI || config.get<string>("database.uri")
 const dbName = config.get<string>("database.name")
+console.log("URI", uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true })
 let connection: MongoClient
 
