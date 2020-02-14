0e015bccfd3fea5b1a11dd661a0763ecba36ed38
Harry Huang <sam.harry.huang@gmail.com>
2020-02-14 01:51:39 -0500

    Added some test data/dev dump

--- scripts/manual_test.sh
+++ scripts/manual_test.sh
@@ -22,11 +22,28 @@ curl -X POST --cookie-jar jarfile 'http://localhost:8000/login' \
 curl -X POST --cookie jarfile 'http://localhost:8000/widgets' \
 -H "Content-Type: application/json" \
 -d '{
-  "name": "Clock",
-  "description": "A clock application",
+  "name": "Mira Weather",
+  "description": "This is the default weather app. Displays local weather conditions and temperatures.",
   "active": false
 }' && echo
 
+curl -X POST --cookie jarfile 'http://localhost:8000/widgets' \
+-H "Content-Type: application/json" \
+-d '{
+  "name": "Mira Clock",
+  "description": "This is the default clock. Basic 7-segment display showing current time.",
+  "active": false
+}' && echo
+
+curl -X POST --cookie jarfile 'http://localhost:8000/widgets' \
+-H "Content-Type: application/json" \
+-d '{
+  "name": "Mira Calender",
+  "description": "Sync with your preferred calendar application and shows your daily agenda.",
+  "active": false
+}' && echo
+
+# Update, should not be successful
 curl -X PUT --cookie jarfile 'http://localhost:8000/widgets/5e08e7d2d8c9b1285e8ebd3e' \
 -H "Content-Type: application/json" \
 -d '{
