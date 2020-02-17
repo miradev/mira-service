97537734a7d41faabbe376cfb497664d376bfa99
Harry Huang <sam.harry.huang@gmail.com>
2020-02-17 10:37:28 -0500

    Added CORS headers, whitelist everything

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,13 @@ import { badObjectId, validate, validateId, validationFailed } from './helpers'
 const app = express()
 const port = config.get<number>('service.port')
 app.use(express.json())
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE')
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type')
+  res.setHeader('Access-Control-Allow-Credentials', 'true')
+  next()
+})
 app.use(
   session({
     // TODO: add to config
